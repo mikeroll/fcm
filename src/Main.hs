e0bbfdcadca448ec4007459385662e06ca16018c
Mikhail Bulash <scaryspiderpig@gmail.com>
2015-09-29 15:30:28 +0300

    Simple csv parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,9 @@
+import System.IO()
+
+-- |'dos2unix' removes those filthy \r's
+dos2unix :: String -> String
+dos2unix = filter (/= '\r')
+
 -- |'split' is like 'words', but splits on given delimiter, not just space
 split     :: Char -> String -> [String]
 split d s = case dropWhile (==d) s of 
@@ -5,6 +11,12 @@ split d s = case dropWhile (==d) s of
                   s' -> w : split d s''
                         where (w, s'') = break (==d) s'
 
+parse_csv   :: String -> IO [[String]]
+parse_csv f = do
+    contents <- readFile f
+    return [split ',' line | line <- lines $ dos2unix contents]
 
-main = print $ split ',' "a,b,c,d"
+main :: IO ()
+main = do
+    parse_csv "input/irises.txt" >>= print
 
