368fc71a1b6f41e25a4748303b38a4a6b59ead60
Mikhail Bulash <scaryspiderpig@gmail.com>
2015-09-29 16:32:17 +0300

    Make a module, check for BOM in input and use FilePath instead of String

---
+++ src/InputUtils.hs
@@ -0,0 +1,28 @@
+module InputUtils 
+( dos2unix 
+, dropBom
+, split
+, parse_csv
+) where
+
+-- |'dos2unix' removes those filthy \r's
+dos2unix :: String -> String
+dos2unix = filter (/= '\r')
+
+-- |'dropBom' drops a UTF-8 BOM if it's present
+dropBom :: String -> String
+dropBom ('\xfeff':s) = s
+dropBom s = s
+
+-- |'split' is like 'words', but splits on given delimiter, not just space
+split     :: Char -> String -> [String]
+split d s = case dropWhile (==d) s of 
+                  "" -> []
+                  s' -> w : split d s''
+                        where (w, s'') = break (==d) s'
+
+-- |'parse_csv' takes a file and reads its contents as csv
+parse_csv   :: FilePath -> IO [[String]]
+parse_csv f = do
+    contents <- readFile f
+    return [split ',' line | line <- lines $ dos2unix $ dropBom contents ]
--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,6 @@
-import System.IO()
-
--- |'dos2unix' removes those filthy \r's
-dos2unix :: String -> String
-dos2unix = filter (/= '\r')
-
--- |'split' is like 'words', but splits on given delimiter, not just space
-split     :: Char -> String -> [String]
-split d s = case dropWhile (==d) s of 
-                  "" -> []
-                  s' -> w : split d s''
-                        where (w, s'') = break (==d) s'
-
-parse_csv   :: String -> IO [[String]]
-parse_csv f = do
-    contents <- readFile f
-    return [split ',' line | line <- lines $ dos2unix contents]
+import InputUtils
 
 main :: IO ()
 main = do
-    parse_csv "input/irises.txt" >>= print
+    parse_csv "input/butterfly.txt" >>= print
 
