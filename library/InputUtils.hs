f022d83292ba2c3293abf0591da2d9c25c6ac096
Mikhail Bulash <scaryspiderpig@gmail.com>
2015-11-08 16:09:35 +0300

    Some example tests

--- library/InputUtils.hs
+++ library/InputUtils.hs
@@ -1,5 +1,5 @@
-module InputUtils 
-( dos2unix 
+module InputUtils
+( dos2unix
 , dropBom
 , split
 , ParseCsvOpts(..)
@@ -18,7 +18,7 @@ dropBom s = s
 
 -- | 'split' is like 'words', but splits on given delimiter, not just space
 split :: Char -> String -> [String]
-split d s = case dropWhile (==d) s of 
+split d s = case dropWhile (==d) s of
     "" -> []
     s' -> w : split d s''
           where (w, s'') = break (==d) s'
@@ -26,7 +26,7 @@ split d s = case dropWhile (==d) s of
 -- | 'parseCsv' takes a file and reads its contents as csv
 parseCsv :: FilePath -> ParseCsvOpts -> IO [[String]]
 parseCsv f opts = do
-    s <- readFile f 
+    s <- readFile f
     return (parseCsvString s opts)
 
 -- | 'parseCsvString' takes a string and reads its contents as csv
@@ -35,7 +35,7 @@ parseCsvString s opts = [getRow line | line <- rows]
     where rows = if stripHeader opts then tail rows' else rows'
                  where rows' = lines . dos2unix . dropBom $ s
           getRow = case (stripNumbering opts, stripClassLabel opts) of
-                      (True, True)   -> init . tail . split d 
+                      (True, True)   -> init . tail . split d
                       (True, False)  -> tail . split d
                       (False, True)  -> init . split d
                       (False, False) -> split d
---
+++ tests/InputUtilsSpec.hs
@@ -0,0 +1,24 @@
+module InputUtilsSpec (spec) where
+
+import Test.Hspec
+import InputUtils
+
+spec :: Spec
+spec = do
+    describe "dos2unix" $ do
+        context "when string has no \r characters" $ do
+            it "returns original string" $ do
+                dos2unix "teststring" `shouldBe` "teststring"
+        context "when string has \r characters" $ do
+            it "returns string with no \r characters" $ do
+                dos2unix "teststring\r" `shouldBe` "teststring"
+
+
+    describe "dropBom" $ do
+        context "when no BOM present" $ do
+            it "returns original string" $ do
+                dropBom "teststring" `shouldBe` "teststring"
+
+        context "when BOM is present" $ do
+            it "returns the string ignoring BOM" $ do
+                dropBom "\xfeffteststring" `shouldBe` "teststring"
